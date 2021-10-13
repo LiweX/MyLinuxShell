294f2a24fa2ea085fee7d996fd787237d8ae96af
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2021-10-13 03:11:15 -0300

    cd en teoria listo

--- internalCmds.c
+++ internalCmds.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
 #include "myFuncs.h"
@@ -8,19 +7,67 @@
 int parseInternalCommands(char* cmd){
 
     char *token = strtok(cmd," ");
+    fflush(stdout);
 
-    if(strstr(token,"quit")){
-        write(0,"\nBye bye!\n",10);
-        exit(EXIT_SUCCESS);
+    if(strstr(token,"cd")){
+
+        char dir[200];
+        token=strtok(NULL,"\n");
+        fprintf(stdout,"%s\n",token);
+        strcpy(dir,getenv("PWD"));
+
+        if(strstr(token,"..")){
+            for(int i=strlen(dir);i>0;i--){
+                if(dir[i]=='/'){
+                    dir[i]='\0';
+                    break;
+                } 
+            }
+            if(chdir(dir) != 0) perror("change to up dir failed");
+            else{
+                setenv("OLDPWD",getenv("PWD"),1);
+                setenv("PWD",dir,1);
+            }
+        }else
+        if(strstr(token,"-")){
+            if(chdir(getenv("OLDPWD")) != 0) perror("change to oldpwd failed");
+            else{
+                setenv("OLDPWD",getenv("PWD"),1);
+                setenv("PWD",getenv("OLDPWD"),1);
+            } 
+        }else
+        if(token[0]=='/'){
+            if(chdir(token) != 0) perror("change direct directory failed");
+            else{
+                setenv("OLDPWD",getenv("PWD"),1);
+                setenv("PWD",token,1);
+            }
+        }else{
+            strcat(dir,"/");
+            strcat(dir,token);
+            if(chdir(dir) != 0) perror("change directory failed");
+            else{
+                setenv("OLDPWD",getenv("PWD"),1);
+                setenv("PWD",dir,1);
+            }
+        }return 0;
     }
     if(strstr(token,"echo")){
-        
-        if(token!=NULL){
-            fflush(stdout);
-            token=strtok(NULL,"\n");
+
+        token=strtok(NULL,"\n");
+        if(token==NULL) return 0;
+        if(token[0]=='$'){
+            char *env = (token+1);
+            if(getenv(env)==NULL) return 0;
+            write(1,getenv(env),strlen(getenv(env)));
+        }else{
             write(1,token,strlen(token));
         }
         return 0;
+    }
+    if(strstr(token,"quit")){
+        write(1,"\nBye bye!\n",10);
+        exit(EXIT_SUCCESS);
     }
     return 1;
 }
